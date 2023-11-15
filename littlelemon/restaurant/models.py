e46bc66a928e1f8ee4e7701468d18d571fb6c3ed
Carlos <carloso2290@gmail.com>
2023-11-15 11:07:59 -0600

    new changes

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    ID = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=255, null=True, default=None)
+    No_of_guests = models.IntegerField(default=0)
+    Bookingdate = models.DateTimeField()
+    
+    class Meta:
+        db_table = "Booking"
+    
+class Menu(models.Model):
+    ID = models.IntegerField(primary_key=True)
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    Inventory = models.IntegerField(default=0)
+    
+    class Meta:
+        db_table = "Menu"    
